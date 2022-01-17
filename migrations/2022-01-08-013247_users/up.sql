0f71ab9cd97530e27685ce8129487f2f2d0307aa
VladislavZh2003 <hanvladi681@gmail.com>
2022-01-17 18:15:20 +0200

    fixed user migration too big primary key

--- migrations/2022-01-08-013247_users/up.sql
+++ migrations/2022-01-08-013247_users/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 
 CREATE TABLE users (
-    u_name VARCHAR(255) PRIMARY KEY,
+    u_name VARCHAR(50) PRIMARY KEY,
     u_email VARCHAR(255) NOT NULL,
     u_password VARCHAR(512) NOT NULL,
     u_created_at DATETIME NOT NULL DEFAULT NOW(),
