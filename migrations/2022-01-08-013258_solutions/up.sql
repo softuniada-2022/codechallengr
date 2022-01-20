df557563cea7428f4b9704339b48beb742bd9322
daki4 <yordanmitev03@protonmail.com>
2022-01-20 16:50:38 +0200

    fix: typo in migrataion

--- migrations/2022-01-08-013258_solutions/up.sql
+++ migrations/2022-01-08-013258_solutions/up.sql
@@ -4,5 +4,5 @@ CREATE TABLE solutions (
   s_id SERIAL PRIMARY KEY,
   ex_id INTEGER NOT NULL,
   u_id INTEGER NOT NULL,
-  submitted_at DATETIME NOT NULL DEFAULT NOW()
+  s_submitted_at DATETIME NOT NULL DEFAULT NOW()
 );
